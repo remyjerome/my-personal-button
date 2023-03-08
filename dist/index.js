a28d2586ce54cd69fa549f156987595bc228f874
remyjerome <remy.jerome@live.com>
2023-03-08 16:44:18 +0100

    modal update

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
+var _Modal = require("./Modal");
+Object.keys(_Modal).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _Modal[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Modal[key];
+    }
+  });
 });
-var _Button = _interopRequireDefault(require("./components/Button"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Modal from './lib/components/Modal';
+import Modal from 'my-personal-button'
 
 function App() {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const handleOpen = () => setIsOpen(true)
+  const handleClose = () => setIsOpen(false)
   return (
     <div className="App">
-      <Modal isOpen={true} allowEscape={true}>
-        <h1>Modal</h1>
+      <button onClick={handleOpen}>Open Modal</button>
+      <Modal isOpen={isOpen} handleClose={handleClose}>
+        <h1>Modal Content</h1>
       </Modal>
     </div>
   );
